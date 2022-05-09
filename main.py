1512a66bc01f76fa978befb7de177ceee03bf8f5
Chris Mitchell <chris.t.mitchell@gmail.com>
2022-05-09 19:54:27 +1200

    Add first movies

---
+++ main.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+movies = [
+    {
+        "name": "movie1",
+        "times": [
+            "2011-11-04T00:05:23",
+            "2011-12-04T00:05:23"
+        ]
+    },
+    {
+        "name": "movie2",
+        "times": [
+            "2011-11-04T00:05:23",
+            "2011-12-04T00:05:23"
+        ]
+    }
+]
+
+print(movies)
