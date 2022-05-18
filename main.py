02d11feebae21291ee1b31aad1184759f3972b37
Chris Mitchell <chris.mitchell@xero.com>
2022-05-18 23:17:14 +1200

    Refactor

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ from datetime import datetime, time
 
 from get_data import get_sessions
 
+from tqdm import tqdm
+
+import random
+
 MAX_SESSIONS = 1
 FAVOURITE_VENUE = "Light House Cuba"
+
+preferences = [
+    {"day": "weekend", "time": "morning"},
+    {"day": "weekend", "time": "afternoon"},
+    {"day": "friday", "time": "evening"},
+    {"day": "weekend", "time": "evening"},
+    {"day": "weekday", "time": "evening"}
+]
 
 all_sessions = []
 
@@ -14,15 +26,15 @@ for session in get_sessions():
 
     match session["date"].weekday():
         case 5 | 6:
-            session["day-bucket"] = "we"
+            session["day-bucket"] = "weekend"
         case 4:
-            session["day-bucket"] = "fri"
+            session["day-bucket"] = "friday"
         case 1 | 2 | 3:
-            session["day-bucket"] = "wd"
+            session["day-bucket"] = "weekday"
         case _:
             session["day-bucket"] = None
 
-    if session["time"] < time(12):
+    if session["time"] < time(13):
         session["time-bucket"] = "morning"
     elif session["time"] < time(17):
         session["time-bucket"] = "afternoon"
@@ -33,28 +45,39 @@ for session in get_sessions():
 
     all_sessions.append(session)
 
-total_schedule = []
+all_schedules = []
 
-def add_session(session):
-    if any(entry['name'] == session["name"] for entry in total_schedule):
-        return
-    if len([x for x in total_schedule if x["date"] == session["date"]]) == MAX_SESSIONS:
-        return
-    total_schedule.append(session)
+def valid_session(session, schedule):
+    if len(schedule) == 0:
+        return True
+    if any(entry["name"] == session["name"] for entry in schedule):
+        return False
+    if len([x for x in schedule if x["date"] == session["date"]]) == MAX_SESSIONS:
+        return False
+    return True
 
-[add_session(session) for session in all_sessions if (session["day-bucket"] == "we" and session["time-bucket"] == "morning" and session["venue"] == FAVOURITE_VENUE)]
-[add_session(session) for session in all_sessions if (session["day-bucket"] == "we" and session["time-bucket"] == "morning")]
+for x in tqdm(range(1000)):
 
-[add_session(session) for session in all_sessions if (session["day-bucket"] == "we" and session["time-bucket"] == "afternoon" and session["venue"] == FAVOURITE_VENUE)]
-[add_session(session) for session in all_sessions if (session["day-bucket"] == "we" and session["time-bucket"] == "afternoon")]
+    current_schedule = []
 
-[add_session(session) for session in all_sessions if (session["day-bucket"] == "fri" and session["time-bucket"] == "evening" and session["venue"] == FAVOURITE_VENUE)]
-[add_session(session) for session in all_sessions if (session["day-bucket"] == "fri" and session["time-bucket"] == "evening")]
+    shuffled_sessions = random.sample(all_sessions, k=len(all_sessions))
 
-[add_session(session) for session in all_sessions if (session["day-bucket"] == "wd" and session["time-bucket"] == "evening" and session["venue"] == FAVOURITE_VENUE)]
-[add_session(session) for session in all_sessions if (session["day-bucket"] == "wd" and session["time-bucket"] == "evening")]
+    for preference in preferences:
+        [current_schedule.append(session) for session in shuffled_sessions if (valid_session(session, current_schedule) and session["day-bucket"] == preference["day"] and session["time-bucket"] == preference["time"] and session["venue"] == FAVOURITE_VENUE)]
+        [current_schedule.append(session) for session in shuffled_sessions if (valid_session(session, current_schedule) and session["day-bucket"] == preference["day"] and session["time-bucket"] == preference["time"])]
 
-for event in sorted(total_schedule, key=lambda x: x["datetime"]):
+    score = 0
+    
+    for position, _ in enumerate(preferences):
+        score += len([session for session in all_sessions if (session["day-bucket"] == preference["day"] and session["time-bucket"] == preference["time"])]) * position
+
+    score += len([session for session in current_schedule if session["venue"] == FAVOURITE_VENUE])
+
+    all_schedules.append({"sessions": current_schedule, "score": score})
+
+best_schedule = sorted(all_schedules, key=lambda item: item["score"], reverse=True)[0]["sessions"]
+
+for event in sorted(best_schedule, key=lambda x: x["datetime"]):
     when = datetime.fromisoformat(event["datetime"]).strftime("%c")
     #print(f"{when}: {event['name']} ({event['venue']}) {event['link']}")
     print(f"{when}: {event['name']} ({event['venue']})")
