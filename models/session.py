c2019d796f0b34abc2191f2f35a37c5d87da36d9
Chris Mitchell <chris.mitchell@xero.com>
2022-07-05 22:49:06 +1200

    Shorten id

--- models/session.py
+++ models/session.py
@@ -18,11 +18,13 @@ class Session:
         self.time_bucket = self.get_time_bucket()
         self.start_time_formatted = datetime.fromisoformat(str(start_time)).strftime(f"%a {self.time_bucket.name.capitalize()} (%d/%m) %I:%M%p")
         self.end_time_formatted = datetime.fromisoformat(str(self.end_time)).strftime("%I:%M%p")
-        self.sesion_id = self.generate_id()
+        self.id = self.generate_id()
 
     def generate_id(self) -> str:
-        combined = (self.film.name + self.venue.name + datetime.fromisoformat(str(self.start_time)).strftime("%d/%m%I:%M%p")).replace(" ", "")
-        return base64.b85encode(combined.encode()).decode()
+        short_name = ''.join([x[0] for x in self.film.name.split(' ')])
+        short_venue = ''.join([x[0] for x in self.venue.name.split(' ')])
+        combined = (short_name + short_venue + datetime.fromisoformat(str(self.start_time)).strftime("%d/%m%I:%M%p")).replace(" ", "")
+        return base64.b64encode(combined.encode()).decode()
 
     def book(self) -> None:
         self.booked = True
