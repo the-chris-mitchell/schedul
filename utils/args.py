84155775298b863fdd6165f4dfbfbc016f7999d9
Chris Mitchell <chris.mitchell@xero.com>
2022-07-05 22:48:05 +1200

    Rename command

--- utils/args.py
+++ utils/args.py
@@ -5,7 +5,7 @@ from festivals import FESTIVALS
 def get_args(festivals) -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("festival", choices=[festival.short_name for festival in festivals], help="Select a festival")
-    parser.add_argument("mode", choices=['sessions', 'schedule', 'films', 'csv', 'cal'], help="What mode to run")
+    parser.add_argument("mode", choices=['sessions', 'schedule', 'films', 'films-csv', 'cal'], help="What mode to run")
     return parser.parse_args()
 
 ARGS = get_args(FESTIVALS)
