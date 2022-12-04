4b863637f5d7aa82398629ce64a628529f9b5ea7
Chris Mitchell <chris.t.mitchell@gmail.com>
2022-12-04 17:00:09 +1300

    Add return type hints

--- models.py
+++ models.py
@@ -5,16 +5,16 @@ from enums import DayBucket, TimeBucket
 
 
 class Film:
-    def __init__(self, name: str, runtime: timedelta):
+    def __init__(self, name: str, runtime: timedelta) -> None:
         self.name = name
         self.runtime = runtime
 
 class Venue:
-    def __init__(self, name: str):
+    def __init__(self, name: str) -> None:
         self.name = name
 
 class Session:
-    def __init__(self, film: Film, venue: Venue, start_time: datetime, link: str):
+    def __init__(self, film: Film, venue: Venue, start_time: datetime, link: str) -> None:
         self.film = film
         self.venue = venue
         self.link = link
@@ -26,17 +26,17 @@ class Session:
         self.start_time_formatted = datetime.fromisoformat(str(start_time)).strftime(f"%a {self.time_bucket.name.capitalize()} (%d/%m) %I:%M%p")
         self.end_time_formatted = datetime.fromisoformat(str(self.end_time)).strftime("%I:%M%p")
 
-    def book(self):
+    def book(self) -> bool:
         self.booked = True
 
-    def format(self):
+    def format(self) -> str:
         return f"{'✅' if self.booked else '👀'} {self.start_time_formatted}-{self.end_time_formatted}: {self.film.name} ({int(self.film.runtime.seconds/60)} minutes) @ {self.venue.name}"
 
 
-    def get_end_time(self):
+    def get_end_time(self) -> datetime:
         return self.start_time + self.film.runtime
 
-    def get_day_bucket(self):
+    def get_day_bucket(self) -> DayBucket:
         match datetime.fromisoformat(str(self.start_time)).date().weekday():
             case 5 | 6:
                 return DayBucket.WEEKEND
@@ -47,7 +47,7 @@ class Session:
             case _:
                 return DayBucket.NONE
 
-    def get_time_bucket(self):
+    def get_time_bucket(self) -> TimeBucket:
         start_time = datetime.fromisoformat(str(self.start_time)).time()
         if start_time < time(13):
             return TimeBucket.MORNING
@@ -59,17 +59,17 @@ class Session:
             return TimeBucket.NONE
 
 class Preference:
-    def __init__(self, time_bucket: TimeBucket=None, day_bucket: DayBucket=None, date: date=None, venue: Venue = None):
+    def __init__(self, time_bucket: TimeBucket=None, day_bucket: DayBucket=None, date: date=None, venue: Venue = None) -> None:
         self.day_bucket = day_bucket
         self.time_bucket = time_bucket
         self.date = date
         self.venue = venue
 
 class Schedule:
-    def __init__(self):
+    def __init__(self) -> None:
         self.sessions: list[Session] = []
     
-    def calculate_score(self, preferences: list[Preference]):
+    def calculate_score(self, preferences: list[Preference]) -> int:
         score = 0
         for position, preference in enumerate(preferences, start=1):
             position_score = 0
