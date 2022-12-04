0bcc15d1cf14f84edc7303d1ec748cfb621a6a22
Chris Mitchell <chris.t.mitchell@gmail.com>
2022-12-04 17:00:09 +1300

    Add booking check back in

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ EXCLUDED_DATES: list[date] = [
     date(2022, 6, 20), # Wellington Film Society
 ]
 
-BOOKED: list[str] = [
+BOOKED_LINKS: list[str] = [
     "https://www.eventcinemas.co.nz/Orders/Tickets#sessionId=1633596&bookingSource=www|movies",
     "https://www.eventcinemas.co.nz/Orders/Tickets#sessionId=1633599&bookingSource=www|movies",
     "https://ticketing.oz.veezi.com/purchase/85571?siteToken=jjyv8y2k8xe3rn5wxqrp2pym64",
@@ -37,13 +37,17 @@ BOOKED: list[str] = [
     "https://ticketing.oz.veezi.com/purchase/85173?siteToken=1qejxwctrta7sf1x8th102760g"
 ]
 
-options = Options(ITERATIONS, MAX_SESSIONS, PREFERENCES, EXCLUDED_DATES)
+options = Options(ITERATIONS, MAX_SESSIONS, PREFERENCES, EXCLUDED_DATES, BOOKED_LINKS)
 
 fff_sessions: list[Session] = get_fff_sessions()
 nziff_sessions: list[Session] = get_nziff_sessions()
 
-fff_schedule: Schedule = get_schedule(options, fff_sessions, "French Film Festival")
-nziff_schedule: Schedule = get_schedule(options, nziff_sessions, "NZIFF")
 
+fff_schedule: Schedule = get_schedule(options, fff_sessions, "French Film Festival")
+nziff_schedule: Schedule = get_schedule(options, nziff_sessions, "Whānau Mārama: NZIFF")
+
+print("=" * 50)
 print_schedule(fff_schedule)
-#print_schedule(nziff_schedule)
+print("=" * 50)
+print_schedule(nziff_schedule)
+print("=" * 50)
