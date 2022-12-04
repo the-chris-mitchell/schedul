a9bd95a71ab8201210f19ea64726677c4b298b5d
Chris Mitchell <chris.t.mitchell@gmail.com>
2022-12-04 17:00:10 +1300

    finishrefactor

--- process.py
+++ process.py
@@ -55,14 +55,3 @@ def get_schedule(options: Options, sessions: list[Session], festival: str) -> Sc
 
     return best_schedule
 
-def print_schedule(schedule: Schedule):
-    print("=" * 50)
-    print(f"🎬 Schedule for {schedule.festival} with {len(schedule.sessions)} films:")
-    print("=" * 50)
-    for position, week in enumerate(sorted(list(set([session.start_time.isocalendar()[1] for session in schedule.sessions]))), start=1):
-        print(f"\n📆 Week {position}:")
-        print("─" * 10)
-        for session in [session for session in schedule.sessions if session.start_time.isocalendar()[1] == week]:
-            print(session.format())
-            if not session.booked:
-                print(f"↪️ 🔗 {session.link}")
