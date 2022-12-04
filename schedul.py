620d1ec968377145da6b2ffc0e81c5b15f3a2d9f
Chris Mitchell <chris.t.mitchell@gmail.com>
2022-12-04 17:00:10 +1300

    Move config to yaml file

--- schedul.py
+++ schedul.py
@@ -1,54 +1,56 @@
 import argparse
 from datetime import date
-from models.enums import DayBucket, TimeBucket
+
+from strictyaml import (Datetime, EmptyList, EmptyNone, Enum,  # type: ignore
+                        Int, Map, Optional, Seq, Str, load)
 
 from festivals.fff import get_sessions as get_fff_sessions
+from festivals.nziff import get_sessions as get_nziff_sessions
+from models.enums import DayBucket, TimeBucket
 from models.options import Options
 from models.preference import Preference
 from models.schedule import Schedule, get_schedule
-from models.venue import Venue
-from festivals.nziff import get_sessions as get_nziff_sessions
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("mode", choices=['print', 'calendar'], help="What mode to run")
 parser.add_argument("festival", choices=['fff', 'nziff'], help="Specify Festival")
 args = parser.parse_args()
 
+preferences_schema = Seq(Map({
+        Optional("venue"): Str(),
+        Optional("time-bucket"): Enum([e.name.lower() for e in TimeBucket]),
+        Optional("day-bucket"): Enum([e.name.lower() for e in DayBucket])
+    }))
 
-MAX_SESSIONS: int = 2
-ITERATIONS: int = 100
+schema = Map({
+    "max-sessions": Int(),
+    "iterations": Int(),
+    "preferences": preferences_schema,
+    Optional("excluded-dates", drop_if_none=False): Seq(Datetime()) | EmptyList() | EmptyNone(),
+    Optional("booked-sessions", drop_if_none=False): Seq(Str()) | EmptyList() | EmptyNone()
+})
+
+with open("config.yaml", "r") as file:
+    config = load(file.read(), schema).data
+
+
+MAX_SESSIONS: int = config["max-sessions"]
+ITERATIONS: int = config["iterations"]
+
+EXCLUDED_DATES: list[date] = [day.date() for day in config["excluded-dates"]]
 
 PREFERENCES: list[Preference] = [
-    Preference(venue=Venue("Embassy Theatre The Grand")),
-    Preference(TimeBucket.MORNING, DayBucket.WEEKEND, venue=Venue("Light House Cuba")),
-    Preference(TimeBucket.AFTERNOON, DayBucket.WEEKEND, venue=Venue("Light House Cuba")),
-    Preference(TimeBucket.MORNING, DayBucket.WEEKEND),
-    Preference(TimeBucket.AFTERNOON, DayBucket.WEEKEND),
-    Preference(TimeBucket.EVENING, DayBucket.WEEKEND),
-    Preference(TimeBucket.EVENING, DayBucket.FRIDAY),
-    Preference(TimeBucket.EVENING, DayBucket.WEEKDAY),
+    Preference(
+        time_bucket=pref.get("time-bucket"),
+        day_bucket=pref.get("day-bucket"),
+        venue=pref.get("venue")
+    )
+    for pref in config["preferences"]
 ]
 
-EXCLUDED_DATES: list[date] = [
-    date(2022, 6, 10), # Parents visiting weekend
-    date(2022, 6, 11), # Parents visiting weekend
-    date(2022, 6, 12), # Parents visiting weekend
-    date(2022, 6, 13), # Wellington Film Society
-    date(2022, 6, 20), # Wellington Film Society
-]
+BOOKED_SESSIONS: list[str] = config["booked-sessions"]
 
-BOOKED_LINKS: list[str] = [
-    "https://www.eventcinemas.co.nz/Orders/Tickets#sessionId=1633596&bookingSource=www|movies",
-    "https://www.eventcinemas.co.nz/Orders/Tickets#sessionId=1633599&bookingSource=www|movies",
-    "https://ticketing.oz.veezi.com/purchase/85571?siteToken=jjyv8y2k8xe3rn5wxqrp2pym64",
-    "https://ticketing.oz.veezi.com/purchase/85561?siteToken=jjyv8y2k8xe3rn5wxqrp2pym64",
-    "https://ticketing.oz.veezi.com/purchase/85575?siteToken=jjyv8y2k8xe3rn5wxqrp2pym64",
-    "https://ticketing.oz.veezi.com/purchase/85173?siteToken=1qejxwctrta7sf1x8th102760g"
-]
-
-options = Options(ITERATIONS, MAX_SESSIONS, PREFERENCES, EXCLUDED_DATES, BOOKED_LINKS)
-
+options = Options(ITERATIONS, MAX_SESSIONS, PREFERENCES, EXCLUDED_DATES, BOOKED_SESSIONS)
 
 schedule: Schedule
 
