cb33954e2fc5adcc0b461737eaa32ac96bb401c8
Chris Mitchell <chris.t.mitchell@gmail.com>
2022-12-04 17:00:09 +1300

    add some logic

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 from datetime import datetime
+import itertools
 
 movies = [
-    {
-        "name": "movie1",
-        "times": [
-            "2011-11-04T00:05:23",
-            "2011-12-04T00:05:23"
-        ]
-    },
-    {
-        "name": "movie2",
-        "times": [
-            "2011-11-04T00:05:23",
-            "2011-12-04T00:05:23"
-        ]
-    }
+    {"name": "movie1", "time": "2011-11-04T00:05:23"},
+    {"name": "movie1", "time": "2011-12-04T00:05:23"},
+    {"name": "movie2", "time": "2011-11-04T00:05:23"},
+    {"name": "movie2", "time": "2011-14-04T00:05:23"},
+    {"name": "movie3", "time": "2011-14-04T00:05:23"},
+    {"name": "movie3", "time": "2011-11-04T00:05:23"},
 ]
 
-print(movies)
+schedules = []       
+
+permutations = itertools.permutations(movies, len(movies))
+
+for permutation in permutations:
+    schedule = {}
+    for p in permutation:
+        movie = p["name"]
+        time = p["time"]
+        if movie in schedule.keys():
+            continue
+        if time in schedule.values():
+            continue
+        schedule[movie] = time
+    schedules.append(schedule)
+
+
+sorted_schedules = sorted(schedules, key=len)
+
+print(sorted_schedules[-1])
