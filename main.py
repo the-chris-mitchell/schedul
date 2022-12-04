dfa58e2c657832d7b5bd7f9ca177791e3129f5a2
Chris Mitchell <chris.t.mitchell@gmail.com>
2022-12-04 17:00:09 +1300

    Refactor logic to process buckets

--- main.py
+++ main.py
@@ -3,48 +3,58 @@ from datetime import datetime, time
 from get_data import get_sessions
 
 MAX_SESSIONS = 1
+FAVOURITE_VENUE = "Light House Cuba"
 
-sessions = []
+all_sessions = []
 
 for session in get_sessions():
     session["date"] = datetime.fromisoformat(session["datetime"]).date()
     session["time"] = datetime.fromisoformat(session["datetime"]).time()
     session["day"] = session["date"].strftime("%A")
 
-    session["score"] = 0
-
-    if session["venue"] == "Light House Cuba":
-        session["score"] += 5
-
     match session["date"].weekday():
         case 5 | 6:
-            if session["time"] < time(17):
-                session["score"] += 10
-            if session["time"] < time(12):
-                session["score"] += 15
+            session["day-bucket"] = "we"
         case 4:
-            session["score"] += 2
-        case 1 | 2 | 3 | 4:
-            if session["time"] > time(18):
-                session["score"] += 1
-        case 0:
-            session["score"] = 0
+            session["day-bucket"] = "fri"
+        case 1 | 2 | 3:
+            session["day-bucket"] = "wd"
+        case _:
+            session["day-bucket"] = None
 
-    sessions.append(session)
+    if session["time"] < time(12):
+        session["time-bucket"] = "morning"
+    elif session["time"] < time(17):
+        session["time-bucket"] = "afternoon"
+    elif session["time"] > time(17):
+        session["time-bucket"] = "evening"
+    else:
+        session["time-bucket"] = "other"
 
-schedule = []
+    all_sessions.append(session)
 
-sorted_sessions = sorted(sessions, key=lambda x: x["score"],reverse=True)
+total_schedule = []
 
-for session in sorted_sessions:
-    if any(entry['name'] == session["name"] for entry in schedule):
-        continue
-    if len([x for x in schedule if x["date"] == session["date"]]) == MAX_SESSIONS:
-        continue
+def add_session(session):
+    if any(entry['name'] == session["name"] for entry in total_schedule):
+        return
+    if len([x for x in total_schedule if x["date"] == session["date"]]) == MAX_SESSIONS:
+        return
+    total_schedule.append(session)
 
-    schedule.append(session)
+[add_session(session) for session in all_sessions if (session["day-bucket"] == "we" and session["time-bucket"] == "morning" and session["venue"] == FAVOURITE_VENUE)]
+[add_session(session) for session in all_sessions if (session["day-bucket"] == "we" and session["time-bucket"] == "morning")]
 
-for event in sorted(schedule, key=lambda x: x["datetime"]):
+[add_session(session) for session in all_sessions if (session["day-bucket"] == "we" and session["time-bucket"] == "afternoon" and session["venue"] == FAVOURITE_VENUE)]
+[add_session(session) for session in all_sessions if (session["day-bucket"] == "we" and session["time-bucket"] == "afternoon")]
+
+[add_session(session) for session in all_sessions if (session["day-bucket"] == "fri" and session["time-bucket"] == "evening" and session["venue"] == FAVOURITE_VENUE)]
+[add_session(session) for session in all_sessions if (session["day-bucket"] == "fri" and session["time-bucket"] == "evening")]
+
+[add_session(session) for session in all_sessions if (session["day-bucket"] == "wd" and session["time-bucket"] == "evening" and session["venue"] == FAVOURITE_VENUE)]
+[add_session(session) for session in all_sessions if (session["day-bucket"] == "wd" and session["time-bucket"] == "evening")]
+
+for event in sorted(total_schedule, key=lambda x: x["datetime"]):
     when = datetime.fromisoformat(event["datetime"]).strftime("%c")
     #print(f"{when}: {event['name']} ({event['venue']}) {event['link']}")
     print(f"{when}: {event['name']} ({event['venue']})")
