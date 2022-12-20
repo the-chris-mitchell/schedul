5f46329cac41b774fe2a8945299d75e9b9bca82f
Chris Mitchell <chris.t.mitchell@gmail.com>
2022-12-21 11:25:52 +1300

    Add type

--- src/schedul/scraper/festivals/base.py
+++ src/schedul/scraper/festivals/base.py
@@ -9,7 +9,7 @@ from services.venue import create_venue_if_required
 
 
 class Festival(ABC):
-    def __init__(self):
+    def __init__(self) -> None:
         self.film_screenings: list[FilmScreening] = []
 
     @property
@@ -45,8 +45,8 @@ class Festival(ABC):
                 start_time=screening.screening_start_time,
                 end_time=screening.screening_start_time + timedelta(minutes=film.runtime),
                 link=screening.screening_link,
-                film_id=film.id,
-                venue_id=venue.id,
-                festival_id=festival.id
+                film_id=film.id, # type: ignore
+                venue_id=venue.id, # type: ignore
+                festival_id=festival.id # type: ignore
             )
         print(f"Loaded {len(self.film_screenings)} screening(s) into DB")
