bc7cc8e5897118e38db558765a28b70f994128c0
Chris Mitchell <chris.t.mitchell@gmail.com>
2023-04-26 20:51:20 +1200

    Connect postgres

--- src/schedul/clients/sql.py
+++ src/schedul/clients/sql.py
@@ -1,6 +1,11 @@
+import os
+
 from sqlmodel import Session, SQLModel, create_engine
 
-engine = create_engine("sqlite:///db.sqlite")
+if db_url := os.getenv("DATABASE_URL"):
+    engine = create_engine(db_url)
+else:
+    engine = create_engine("sqlite:///db.sqlite")
 
 
 def create_db_and_tables():
