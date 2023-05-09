1a63c80caf6f6da07ad22852b8a8a903dd0099a3
Chris Mitchell <chris.t.mitchell@gmail.com>
2023-05-09 22:07:02 +1200

    A not so working frontend

---
+++ src/frontend/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  productionBrowserSourceMaps: true
+}
+
+export default nextConfig
