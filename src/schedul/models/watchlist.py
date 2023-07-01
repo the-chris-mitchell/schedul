259c49348dd5e622786d52f1743be05f79a81a80
Chris Mitchell <chris.t.mitchell@gmail.com>
2023-07-01 21:18:53 +1200

    Move things around

--- src/schedul/models/watchlist.py
+++ src/schedul/models/watchlist.py
@@ -1,7 +1,11 @@
+import uuid as uuid_pkg
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
+
+from sqlmodel import Field, Relationship, SQLModel
 
 from models.film import Film
+from models.user import User
 
 
 @dataclass
@@ -13,3 +17,24 @@ class WatchlistFilm:
 @dataclass
 class Watchlist:
     films: List[WatchlistFilm]
+
+
+class WatchlistEntryBase(SQLModel):
+    user_uuid: uuid_pkg.UUID = Field(foreign_key="user.uuid")
+    film_id: int = Field(foreign_key="film.id")
+
+
+class WatchlistEntry(WatchlistEntryBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user: User = Relationship()
+    film: Film = Relationship()
+
+
+class WatchlistEntryCreate(WatchlistEntryBase):
+    pass
+
+
+class WatchlistEntryRead(WatchlistEntryBase):
+    id: int
+    user: User
+    film: Film
