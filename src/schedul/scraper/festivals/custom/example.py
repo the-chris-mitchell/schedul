adeb440c097f593c7ef198086e81ec7b4483c418
Chris Mitchell <chris.t.mitchell@gmail.com>
2023-07-01 16:36:20 +1200

    Update example to use current api

--- src/schedul/scraper/festivals/custom/example.py
+++ src/schedul/scraper/festivals/custom/example.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timezone
 
+from models.film import FilmBase
 from models.film_screening import FilmScreening
+from models.venue import VenueBase
 from scraper.festivals.base import Festival
 
 
@@ -14,12 +16,10 @@ class TestFest(Festival):
         return "tf"
 
     def scrape(self):
-        film1 = FilmScreening(
-            film_name="Citizen Kane",
-            film_runtime=119,
-            film_year=1941,
-            venue_name="Embassy Grand",
-            screening_start_time=datetime.now(timezone.utc),
+        screening = FilmScreening(
+            film=FilmBase(name="Citizen Kane", runtime=119),
+            venue=VenueBase(name="Embassy Grand"),
+            screening_start_time_utc=datetime.now(timezone.utc),
             screening_link="https://example.com",
         )
-        self.film_screenings.append(film1)
+        self.film_screenings.append(screening)
