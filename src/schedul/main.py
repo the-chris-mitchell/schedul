13bc47e91f6e2dd1c030f8b0a6a69a1404ead646
Chris Mitchell <chris.t.mitchell@gmail.com>
2024-06-11 20:36:39 +1200

    Refactor to handle deprecation of on_event handler

--- src/schedul/main.py
+++ src/schedul/main.py
@@ -1,11 +1,21 @@
-import uvicorn
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from clients.sql import create_db_and_tables
 from routers import bookings, festivals, films, screenings, users, venues, watchlist
 
-app: FastAPI = FastAPI(title="Schedul", docs_url=None, redoc_url="/docs")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app: FastAPI = FastAPI(
+    title="Schedul", docs_url=None, redoc_url="/docs", lifespan=lifespan
+)
 
 origins: list[str] = ["http://localhost:3000"]
 
@@ -18,11 +28,6 @@ app.add_middleware(
 )
 
 
-@app.on_event("startup")
-def on_startup() -> None:
-    create_db_and_tables()
-
-
 app.include_router(films.router)
 app.include_router(screenings.router)
 app.include_router(venues.router)
@@ -30,6 +35,3 @@ app.include_router(festivals.router)
 app.include_router(users.router)
 app.include_router(watchlist.router)
 app.include_router(bookings.router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
