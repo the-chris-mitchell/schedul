a7b6b54086742c6de6951e3aac0c8c6772a58e65
Chris Mitchell <chris.t.mitchell@gmail.com>
2025-07-01 21:26:01 +1200

    Switch to click

--- src/schedul/scrape.py
+++ src/schedul/scrape.py
@@ -1,34 +1,29 @@
-import argparse
+import click
 
 from schedul.clients.sql import create_db_and_tables
 from schedul.scraper.festivals import FESTIVALS
 from schedul.scraper.festivals.base import Festival
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
+
+@click.command()
+@click.argument(
     "env",
-    choices=["dev", "prod"],
-    help="Select an environment",
+    type=click.Choice(["dev", "prod"]),
 )
-parser.add_argument(
+@click.argument(
     "festival",
-    choices=[festival.short_name for festival in FESTIVALS],
-    help="Select a festival",
+    type=click.Choice([festival.short_name for festival in FESTIVALS]),
 )
-ARGS = parser.parse_args()
+def main(env: str, festival: str):
+    selected_festival: Festival = [
+        fest for fest in FESTIVALS if fest.short_name == festival
+    ][0]
 
-
-selected_festival: Festival = [
-    festival for festival in FESTIVALS if festival.short_name == ARGS.festival
-][0]
-
-
-def main():
-    if ARGS.env == "dev":
+    if env == "dev":
         create_db_and_tables()
         print(f"Scraping: {selected_festival.full_name}")
         selected_festival.create_resources_dev()
-    if ARGS.env == "prod":
+    if env == "prod":
         selected_festival.create_resources_prod()
 
 
